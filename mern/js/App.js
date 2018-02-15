3c5619e9bcf38b63c120727d54b233a0b42bc2e4
Robert Dry <s26405d@DEVM021.local>
2018-02-15 16:13:49 -0500

    updates to mern and svg work

--- mern/js/App.js
+++ mern/js/App.js
@@ -37,7 +37,7 @@ class BorderWrap extends React.Component {
 ///////////////////////////////////////////////////////////////// ISSUE ROW FUNCTION (STATELESS COMPONENT)
 const IssueRow = (props) => (
         <tr>
-            <td>{props.issue.id}</td>
+            <td>{props.issue._id}</td>
             <td>{props.issue.status}</td>
             <td>{props.issue.owner}</td>
             <td>{props.issue.created.toDateString()}</td>
@@ -49,7 +49,7 @@ const IssueRow = (props) => (
 
 ///////////////////////////////////////////////////////////////// ISSUE TABLE FUNCTION (STATELESS COMPONENT)
 function IssueTable(props) {
-    const issueRows = props.issues.map(issue =><IssueRow key={issue.id} issue={issue} />);                                             
+    const issueRows = props.issues.map(issue =><IssueRow key={issue._id} issue={issue} />);                                             
             return (
             <table className="bordered-table">
             <thead>
@@ -118,18 +118,21 @@ class IssueList extends React.Component {
     }
     
     loadData() {
-        fetch('/api/issues').then(response => response.json()).then(data => {
+        fetch('/api/issues').then(response => 
+            if (response.ok) {
+            response.json()).then(data => {
             console.log("Total count of records:",data._metadata.total_count);
             data.records.forEach(issue => {
                 issue.created = new Date(issue.created);
                 if (issue.completionDate)
                     issue.completionDate = new Date(issue.completionDate);
             });
-            this.setState({
-                issues: data.records
+            } else {
+                response.json().then(error => {
+                alert("Failed to fetch issues:" + error.message)
             });
-        }).catch(err => {
-            console.log(err);
+        }
+        }).catch(err => {alert("Error in fetching data from server:", err);
         });
     }
     
--- mern/js/server.js
+++ mern/js/server.js
@@ -1,38 +1,44 @@
 //const ances
-
 const express = require('express');
 
 const app = express();
 
 const bodyParser = requires('body-parser');
 
-const issues = [
-    {
-        id: 1, status: 'Open', owner: 'Ravan', created: new Date('2016-08-15'), effort: 5, completionDate: undefined, title: 'Error in console when clicking Add',
-    },
-    {
-        id: 2, status: 'Assigned', owner: 'Eddie', created: new Date('2016-08-16'), effort: 14, completionDate: new Date('2016-08-30'), title: 'Missing bottom border on panel',
-    },
-];
-
 //apps
-
 app.use(express.static('static'));
 app.use(bodyParser.json());
 
 app.get('/api/issues', (req,res) => {
-    const metadata = { total_count: issues.length };
-    res.json( { _metadata: metadata, records: issues});
+    db.collection('issues').find().toArray().then(issues => {
+        const metadata = {total_count:issues.length};
+        res.json({_metadata: metadata, records:issues})
+    }).catch(error => {
+        console.log(error);
+        res.status(500).json({message:'Internal Server Error: ${error}'});
+    });
 });
 
 app.listen(3000, () => {
     console.log('App started on port 3000');
 }); 
 
-//error handling
+//connect to mongo db
+const MongoClient = require('mongodb').MongoClient;
 
+let db;
+MongoClient.connect('mongodb://localhost/issuetracker').then(connection => {
+    db = connection;
+    app.listen(3000, () => {
+        console.log('App started on port 3000');
+    });
+}).catch(error => {
+    console.log('ERROR:', error);
+});
+
+//error handling
 const validIssueStatus = { New: true, Open: true, Assigned: true, Fixed: true, Verified: true, Closed: true, }; 
-const issueFieldType = { id: 'required', status: 'required', owner: 'required', effort: 'optional',created: 'required', completionDate: 'optional', title: 'required', }; 
+const issueFieldType = { status: 'required', owner: 'required', effort: 'optional',created: 'required', completionDate: 'optional', title: 'required', }; 
 
 function validateIssue(issue) { 
     for (const field in issueFieldType) { 
@@ -44,9 +50,9 @@ function validateIssue(issue) {
         if (!validIssueStatus[issue.status]) return `${issue.status} is not a valid status.`; return null; 
 } 
 
+//Create API
 app.post('/api/issues', (req, res) => { 
         const newIssue = req.body; 
-        newIssue.id = issues.length + 1; 
         newIssue.created = new Date(); 
         if (!newIssue.status) 
         newIssue.status = 'New'; 
@@ -56,6 +62,13 @@ app.post('/api/issues', (req, res) => {
                 res.status(422).json({ message: `Invalid requrest: ${err}` });return; 
             } 
             //error handling
-        issues.push(newIssue); 
-        res.json(newIssue); 
+        db.collectionn('issues').insertOne(newIssue).then(result => db.collection('issues').find({
+            _id: result.insertID
+        }).limit(1).next()
+        ).then(newIssue => {
+            res.json(newIssue);
+        }).catch(error => {
+            console.log(error);
+            res.status(500).json({message: 'Internal Server Error: ${error}'});          
+        });
 });
