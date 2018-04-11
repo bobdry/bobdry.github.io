5fed191fc88c5fad3429d24090e0923a8d8f763f
Robert Dry <s26405d@DEVM021.local>
2018-04-11 15:28:00 -0400

    change to lunr js for gitbug json path

--- lunrRedux/js/lunrFunc.js
+++ lunrRedux/js/lunrFunc.js
@@ -6,7 +6,7 @@ var docs = [];
 var DoSearch = function () {
     
 //get JSON
-    $.getJSON("/js/lunrIndex.json", function (data) {
+    $.getJSON("/lunrRedux/js/lunrIndex.json", function (data) {
     
     //and loop though JSON to populate docs array   
     $.each( data, function(key) {
