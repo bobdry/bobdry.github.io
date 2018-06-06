7a6b9fcfad8d548b6fd4b08f9b75edf65d5a5f6b
Robert Dry <s26405d@DEVM021.local>
2018-06-06 16:48:05 -0400

    json jsx work, finally got it to render nested arrays

--- jsonRedux/js/json.jsx
+++ jsonRedux/js/json.jsx
@@ -23,7 +23,7 @@ function createCORSRequest(method, url) {
 function makeCorsRequest() {
   // This is a sample server that supports CORS.
   //var url = 'http://html5rocks-cors.s3-website-us-east-1.amazonaws.com/index.html';
-  var url = 'http://127.0.0.1:55074/jsonRedux/js/jsonTemp.json'
+  var url = 'http://127.0.0.1:51883//jsonRedux/js/jsonTemp.json'
   var xhr = createCORSRequest('GET', url);
   if (!xhr) {
     alert('CORS not supported');
@@ -45,67 +45,52 @@ function pushRenderData(data){
     $.each( data, function(key) {
         posts.push( data[key] );
     });
-    console.log(posts);
-    //data feeds into react component and renders
+    //console.log(posts);
+    //data feeds into react function and renders
     reactJsonCorsProps();
 }
 
 //react component for JSON makeCorsRequest using PROPS
 function reactJsonCorsProps() {
     //react function for constants with loops
-    
-    var itemOneLength = posts.length;
-
-    //Make Arrs for each level of the JSON and push in the values that need to be  rendered
-    //Arr holders
-    const itemOneArr = [];   
-    const itemTwoArr = [];
-    const itemThreeArr = [];
-    
-    for (var i=0; i<itemOneLength; i++) {
-        itemOneArr.push(posts[i])
-        var itemTwoLength = posts[i].itemOnechildren.length
-        for (var j=0; j<itemOneLength; j++) {
-            itemTwoArr.push(posts[i].itemOnechildren[j])
-            var itemThreeLength = posts[i].itemOnechildren[j].itemTwochildren.length
-                for (var p=0; p<itemOneLength; p++) {
-                itemThreeArr.push(posts[i].itemOnechildren[j].itemTwochildren[p])
-            }
-        }
-    }
-    console.log(itemOneArr);
-    console.log(itemTwoArr);
-    console.log(itemThreeArr);
-
     function JsonRender(props) {  
   
-        const itemThreeArrFunc = itemThreeArr.map((item) =>
-            <ul key={item.itemThree}>
-                <li>{item.itemThree}</li>
-            </ul>
-        );
-
-        const itemTwoArrFunc = itemTwoArr.map((item) =>
-            <ul key={item.itemTwo}>
-                <li>{item.itemTwo}</li>
-                    {itemThreeArrFunc}
-            </ul>
-        );
-                      
         const JsonRenderLoop = (
         <div>
         <h4>Menu</h4>
         <ul>
-            {props.posts.map((post) =>
+            {props.posts.map((post, i) =>
             <li key={post.itemOne}>
             {post.itemOne}
-            {itemTwoArrFunc}
+            {
+            post.itemOnechildren.map((subpost, j) => {
+                return (
+                <ul key={subpost.itemTwo}>
+                <li>
+                {subpost.itemTwo}
+                {
+                    subpost.itemTwochildren.map((subpostsub, k) => {
+                    return (
+                    <ul key={subpostsub.itemThree}>
+                    <li>
+                    {subpostsub.itemThree}
+                    <p>{subpostsub.itemThreedetails}</p>
+                    </li>
+                    </ul>
+                    )
+                    })
+                }              
+                </li>
+                </ul>
+                )
+                })
+            }
             </li>
             )}
         </ul>
         </div>
         );
-        
+    
         
     return (
         <div>
@@ -118,5 +103,6 @@ function reactJsonCorsProps() {
     destinationOne
     );  
 }
+
 //call funcs to build component
 makeCorsRequest(pushRenderData);
